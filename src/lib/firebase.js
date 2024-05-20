c14a07fb61fca917619ac8fc9f3354ad30788abe
Rayy-007 <minkhant88887@gmail.com>
2024-05-20 17:06:48 +0630

    feat: add evn file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNciTK7GwpnM3vTjc89Ba9LvipSXFdklg",
-  authDomain: "poc-challenge-36c44.firebaseapp.com",
-  projectId: "poc-challenge-36c44",
-  storageBucket: "poc-challenge-36c44.appspot.com",
-  messagingSenderId: "156971086530",
-  appId: "1:156971086530:web:85a3caee44cc51f086c15b",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
